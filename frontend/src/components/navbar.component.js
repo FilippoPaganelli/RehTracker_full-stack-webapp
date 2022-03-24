51ed401e66e454936d5bb60cefc4e13c02a97678
Filippo Paganelli <paganellifilippo@gmail.com>
2022-03-24 18:17:24 +0100

    changed some names

--- frontend/src/components/navbar.component.js
+++ frontend/src/components/navbar.component.js
@@ -6,14 +6,14 @@ export default class Navbar extends Component {
   render() {
     return (
       <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-        <Link to="/" className="navbar-brand">ExcerTracker</Link>
+        <Link to="/" className="navbar-brand">RehTracker</Link>
         <div className="collpase navbar-collapse">
         <ul className="navbar-nav mr-auto">
           <li className="navbar-item">
-          <Link to="/" className="nav-link">Exercises</Link>
+          <Link to="/" className="nav-link">See Exercises</Link>
           </li>
           <li className="navbar-item">
-          <Link to="/create" className="nav-link">Create Exercise Log</Link>
+          <Link to="/create" className="nav-link">Create Exercise</Link>
           </li>
           <li className="navbar-item">
           <Link to="/patient" className="nav-link">Create Patient</Link>
