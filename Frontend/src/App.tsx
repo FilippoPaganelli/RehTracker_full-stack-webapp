517606073f48a70e23f9dd7ca2714c508fecbb28
FilippoPaganelli <32205866+FilippoPaganelli@users.noreply.github.com>
2023-09-17 23:03:00 +0200

    minor fixes

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,12 +1,9 @@
 import React from 'react'
-import axios from 'axios'
 import { AuthContextProvider } from './contexts'
 import { Router } from './components'
 
 import 'react-day-picker/dist/style.css'
 import './App.css'
-
-axios.defaults.withCredentials = true
 
 function App() {
 	return (
